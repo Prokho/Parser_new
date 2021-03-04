cc6fbbae7b3c74ad34860726e659cbb0c9941d7b
Prokhor Proshkin <pproshkin@yandex.ru>
2021-03-04 20:07:10 +0300

    !!!

--- Parcer_new.py
+++ Parcer_new.py
@@ -1,9 +1,16 @@
 import requests
-import select
-
-res = requests.get("https://www.kommersant.ru/doc/4712725?utm_source=yxnews&utm_medium=desktop")
-
-f = open("data.html", "w", encoding="utf-8")
-f.write(res.text)
-
-print(res.text)
+run = True
+id = 4712725
+max_id=4712900
+while run and id < max_id:
+    print(id)
+    res = requests.get(f"https://www.kommersant.ru/doc/{id}")
+    if res.status_code==200:
+        f = open(f"{id}.html", "w", encoding="utf-8")
+        f.write(res.text)
+        f.close()
+        if res.text.find("Доступ к запрашиваемой странице закрыт") != -1:
+            run =False
+ #   else:
+ #       run=False
+    id +=1
