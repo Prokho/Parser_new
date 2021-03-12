fb5c1bd9cfb3a1996273065c1f1b32d128417777
Prokhor Proshkin <pproshkin@yandex.ru>
2021-03-12 18:54:54 +0300

    !!!

---
+++ parcer_izvestia.py
@@ -0,0 +1,16 @@
+import requests
+run = True
+id = 1101000
+max_id=1135300
+while run and id < max_id:
+    print(id)
+    res = requests.get(f"https://iz.ru/{id}")
+    if res.status_code==200:
+        f = open(f"{id}.html", "w", encoding="utf-8")
+        f.write(res.text)
+        f.close()
+        if res.text.find("Доступ к запрашиваемой странице закрыт") != -1:
+            run =False
+ #   else:
+ #       run=False
+    id +=1
